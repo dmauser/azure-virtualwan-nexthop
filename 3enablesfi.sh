7dd0fce0f97e3871d26f97810a7dd9bea3abd6f5
Daniel Mauser <dmauser@hotmail.com>
2025-06-17 17:32:01 -0500

    Update 3enablesfi.sh

--- 3enablesfi.sh
+++ 3enablesfi.sh
@@ -9,3 +9,5 @@ az vm run-command invoke -g $rg -n spoke2-linux-nva1 --command-id RunShellScript
 scripturi="https://raw.githubusercontent.com/dmauser/azure-virtualwan-nexthop/refs/heads/main/scripts/iptables.sh"
 az vm run-command invoke -g $rg -n spoke2-linux-nva2 --command-id RunShellScript --scripts "curl -s $scripturi | bash" --output none --no-wait
 
+echo The command has completed sucessfully.
+

591f817a7981ada02a460ab1128cfd19614e1563
Daniel Mauser <dmauser@hotmail.com>
2025-06-28 21:35:06 -0500

    Update 3enablesfi.sh

--- 3enablesfi.sh
+++ 3enablesfi.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 rg=lab-vwan-nexthop
 
+echo "Enabling stateful inspection on Linux NVA VMs in $rg..."
 # Enable IP Tables 
+echo "Enabling stateful inspection on spoke1-linux-nva1..."
 scripturi="https://raw.githubusercontent.com/dmauser/azure-virtualwan-nexthop/refs/heads/main/scripts/iptables.sh"
 az vm run-command invoke -g $rg -n spoke2-linux-nva1 --command-id RunShellScript --scripts "curl -s $scripturi | bash" --output none --no-wait
 
 # Enable IP Tables 
+echo "Enabling stateful inspection on spoke1-linux-nva2..."
 scripturi="https://raw.githubusercontent.com/dmauser/azure-virtualwan-nexthop/refs/heads/main/scripts/iptables.sh"
 az vm run-command invoke -g $rg -n spoke2-linux-nva2 --command-id RunShellScript --scripts "curl -s $scripturi | bash" --output none --no-wait
 
 echo The command has completed sucessfully.
-
